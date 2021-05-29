aab8238d1cf95b2214688cb68bf830633d750acc
marqun <35145079+marqun@users.noreply.github.com>
2021-05-29 09:31:06 +0200

    problem 6
    Solved

--- sol6.py
+++ sol6.py
@@ -1,9 +1,16 @@
 """The sum of the squares of the first ten natural numbers is,
-
 The square of the sum of the first ten natural numbers is,
-
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
-
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
+x = 100
+y = 0
+z = 0
+for i in range(1,x+1):
+    y+= (i*i)
+    #print("i:", i , "y:", y)
+    z+=i
+    if i == x:
+        z= z*z
+        #print("z:", z)
 
-
+print("solution:", z-y)
