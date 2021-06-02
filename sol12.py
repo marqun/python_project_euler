78a075302df33986cbb993b5b464c55555c42b6e
marqun <35145079+marqun@users.noreply.github.com>
2021-06-02 20:21:10 +0200

    sol11
    data loaded

---
+++ sol12.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+zeors_array = np.zeros( (2, 3) )
+
+
+
+print(zeors_array)
+""""column = [];        # empty list
+for row in A:
+  column.append(row[2])   
+
+print("3rd column =", column)"""
