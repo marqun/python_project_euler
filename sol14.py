034f7b420480bee49f8c64a29f6b0c99bc7baf92
marqun <35145079+marqun@users.noreply.github.com>
2021-06-05 20:11:22 +0200

    Update sol14.py
    working, little slow

--- sol14.py
+++ sol14.py
@@ -7,23 +7,30 @@ It can be seen that this sequence (starting at 13 and finishing at 1) contains 1
 Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
 Which starting number, under one million, produces the longest chain?
 NOTE: Once the chain starts the terms are allowed to go above one million."""
+
+import time
+start_time = time.time()
+
 lmax = 1
 jmax = 1
-for j in range(1, 100):
+kmax = 1
+for j in range(1, 1000000):
     l = 1
-    k=j
-    print("\nj:", j)
+    k = j
+    #print("\nj:", j)
     while j != 1:
         if j % 2 == 0:
             j=j/2
-            print("j:", j)
+            #print("j:", j)
             l += 1
         elif j % 2 != 0:
             j=3*j+1
-            print("j:", j)
+            #print("j:", j)
             l+= 1
-        if j == 1: print("l:", l, "lmax:", lmax)
         if l>lmax :
+            kmax = k
             lmax = l
-            jmax = k
-print("k:", k, "lmax:", lmax)
+
+print("lmax:", lmax, "kmax", kmax)
+y = (time.time() - start_time)
+print("--- %s seconds ---" %y)
